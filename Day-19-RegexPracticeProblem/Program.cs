cc07c1aff0000dd75e51e3dc94cd66a1946ac5bc
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-08 22:29:36 +0530

    [Ronit] Added : First Name

---
+++ Day-19-RegexPracticeProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_19_RegexPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression");
+            string FirstName = Console.ReadLine();
+            Validation.FirstNameValidation(FirstName);
+
+
+        }
+    }
+}
---
+++ Day-19-RegexPracticeProblem/Validation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day_19_RegexPracticeProblem
+{
+    internal class Validation
+    {
+        public static void FirstNameValidation(string FirstName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Creating REGEX pattern
+            if (Regex.IsMatch(FirstName, pattern))
+            {
+                Console.WriteLine("The First name is : " +FirstName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Name : "+FirstName);
+            }
+        }
+    }
+}
