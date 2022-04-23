65adb28e58bf96ec16c7801757ffbeb28101a13f
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-04-23 23:44:21 +0530

    [Ronit]Added:Parameterised Test to validate multiple entry for the Email Address

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -29,19 +29,42 @@ namespace UnitTest
             string actual = Validation.LastNameValidation(massage);
             //Assert
             Assert.AreEqual(expected, actual);
-
         }
+        /// <summary>
+        /// Emails the validation.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <param name="expected">The expected.</param>
         [TestMethod]
-        public void EmailValidation()
+        //Checking for multiple email samples that are valid
+        [DataRow("abc@gmail.com", "Valid")]
+        [DataRow("abc-100@yahoo.com", "Valid")]
+        [DataRow("abc.100@yahoo.com", "Valid")]
+        [DataRow("abc@1.com", "Valid")]
+        [DataRow("abc111@yahoo.com.au", "Valid")]
+        [DataRow("abc-100@yahoo.com.au", "Valid")]
+        [DataRow("abc@gmail.com.com", "Valid")]
+        [DataRow("abc+100@yahoo.com", "Valid")]
+        //Checking for multiple email samples that are Invalid
+        [DataRow("abc", "Invalid")]
+        [DataRow("abc@.com.my", "Invalid")]
+        [DataRow("abc123@gmail.a", "Invalid")]
+        [DataRow("abc123@.com", "Invalid")]
+        [DataRow("abc@.com.com", "Invalid")]
+        [DataRow(".abc@abc.com", "Invalid")]
+        [DataRow("abc()*@gmail.com", "Invalid")]
+        [DataRow("abc@%*.com", "Invalid")]
+        [DataRow("abc..2002@gmail.com", "Invalid")]
+        [DataRow("abc.@gmail.com", "Invalid")]
+        [DataRow("abc@abc@gmail.com", "Invalid")]
+        [DataRow("abc@gmail.com.1a", "Invalid")]
+        [DataRow("abc@gmail.com.aa.au", "Invalid")]
+        public void EmailValidation(string email,string expected)
         {
-            //Arrange
-            string expected = "Valid";
-            string massage = "ronitkumatpatel83@gmail.com";
             //Act
-            string actual = Validation.EmailValidation(massage);
+            string actual = Validation.EmailValidation(email);
             //Assert
             Assert.AreEqual(expected, actual);
-
         }
         [TestMethod]
         public void MobileNumber()
